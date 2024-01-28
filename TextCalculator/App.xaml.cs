22600c0225f7aa9dd12f57d0f34f8f6d05785cbb
KaiHuaDou <419437394@qq.com>
2024-01-28 11:42:39 +0800

    :sparkles: 增加单文件发布

--- TextCalculator/App.xaml.cs
+++ TextCalculator/App.xaml.cs
@@ -1,6 +1,35 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using SingleInstanceCore;
 
 namespace TextCalculator;
-public partial class App : Application
+public partial class App : Application, ISingleInstance
 {
+    public static class Program
+    {
+        [STAThread]
+        public static void Main( )
+        {
+            App app = new( );
+            app.InitializeComponent( );
+            app.Run( );
+        }
+    }
+
+    public void OnInstanceInvoked(string[] args)
+    {
+        Current.MainWindow.Topmost = !Current.MainWindow.Topmost;
+        Current.MainWindow.Topmost = !Current.MainWindow.Topmost;
+    }
+
+    private void AppStartup(object sender, StartupEventArgs e)
+    {
+        if (!this.InitializeAsFirstInstance("TextCalculator_1_0_0"))
+            Current.Shutdown( );
+    }
+
+    private void AppExit(object sender, ExitEventArgs e)
+    {
+        SingleInstance.Cleanup( );
+    }
 }
